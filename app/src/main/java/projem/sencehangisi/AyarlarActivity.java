337925545f66b2c61478d46c5c96484246d5eb78
Gökhan Akbaba <gokhanakbaba1907@gmail.com>
2018-03-05 12:51:59 +0300

    Ayarlar Activity
    Ayarlar ekran tasarımı yapıldı

--- app/src/main/java/projem/sencehangisi/AyarlarActivity.java
+++ app/src/main/java/projem/sencehangisi/AyarlarActivity.java
@@ -1,8 +1,9 @@
 package projem.sencehangisi;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
 
 public class AyarlarActivity extends AppCompatActivity {
 
@@ -12,5 +13,11 @@ public class AyarlarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ayarlar);
         ActionBar actionBar=getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void KullaniciAdiDegisim(View view)
+    {
+        AyarlarKullaniciAdi ayarlarKullaniciAdi=new AyarlarKullaniciAdi();
+        getSupportFragmentManager().beginTransaction().replace(R.id.AyarlarLayout,ayarlarKullaniciAdi).commit();
     }
 }

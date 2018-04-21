15796a0814f38566a9d026be500bfeefd7e56ef1
Gökhan Akbaba <gokhanakbaba1907@gmail.com>
2018-04-21 21:43:12 +0300

    Kullanıcı Profili ve Giriş Ekranı
    Kullanıcı Profili ve Giriş Ekranı Tasarımları yenilendi.

--- app/src/main/java/projem/sencehangisi/fragments/KullaniciProfiliActivity.java
+++ app/src/main/java/projem/sencehangisi/fragments/KullaniciProfiliActivity.java
@@ -1,5 +1,6 @@
 package projem.sencehangisi.fragments;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
@@ -9,7 +10,6 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.TextView;
 
 import com.android.volley.Response;
@@ -32,17 +32,20 @@ public class KullaniciProfiliActivity extends AppCompatActivity {
     @BindView(R.id.images) CircleImageView mImageView;
     private UserInfo userInfo;
     private OturumYonetimi userSession;
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_kullanici_profili);
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+       getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
-        ActionBar actionBar=getSupportActionBar();
+       ActionBar actionBar=getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+       actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#40000000")));
 
         ButterKnife.bind(this);
         userInfo = new UserInfo(this);
